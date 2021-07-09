323d0afe5e99bd27752ca178916de57172ec6b32
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-07-09 13:48:05 +0200

    Updated documentation for functions to coerce from messy dates to other date classes

--- R/messydate_to.R
+++ R/messydate_to.R
@@ -1,43 +1,65 @@
-#' Coercion from messydate
+#' Coercion from messydate to date
+#'
+#' Coerces from messydt class to date class
+#' @param x a messydt object
+#' @param resolve How do you want the date ranges to be resolved?
+#' Before coercing dates, one should chose how to resolve ranges
+#' so that dates can be coerced to other date classes.
+#' Choices are "min","max","median", or "mean".
 #' @export
-as.Date.messydt <- function(x, 
+as.Date.messydt <- function(x,
                             resolve = c("min","max","median","mean")){
-  
+
   resolve <- match.arg(resolve)
-  
+
   if(resolve == "min") x <- min(x)
   if(resolve == "max") x <- max(x)
   if(resolve == "mean") x <- mean(x)
   if(resolve == "median") x <- median(x)
-  
+
   as.Date(x)
 }
 
+#' Coercion from messydate to POSIXct
+#'
+#' Coerces from messydt class to POSIXct class
+#' @param x a messydt object
+#' @param resolve How do you want the date ranges to be resolved?
+#' Before coercing dates, one should chose how to resolve ranges
+#' so that dates can be coerced to other date classes.
+#' Choices are "min","max","median", or "mean".
 #' @export
-as.POSIXct.messydt <- function(x, 
+as.POSIXct.messydt <- function(x,
                                resolve = c("min","max","median","mean")){
-  
+
   resolve <- match.arg(resolve)
-  
+
   if(resolve == "min") x <- min(x)
   if(resolve == "max") x <- max(x)
   if(resolve == "mean") x <- mean(x)
   if(resolve == "median") x <- median(x)
-  
+
   as.POSIXct(x)
 }
 
+#' Coercion from messydate to POSIXlt
+#'
+#' Coerces from messydt class to POSIXlt class
+#' @param x a messydt object
+#' @param resolve How do you want the date ranges to be resolved?
+#' Before coercing dates, one should chose how to resolve ranges
+#' so that dates can be coerced to other date classes.
+#' Choices are "min","max","median", or "mean".
 #' @export
-as.POSIXlt.messydt <- function(x, 
+as.POSIXlt.messydt <- function(x,
                                resolve = c("min","max","median","mean")){
-  
+
   resolve <- match.arg(resolve)
-  
+
   if(resolve == "min") x <- min(x)
   if(resolve == "max") x <- max(x)
   if(resolve == "mean") x <- mean(x)
   if(resolve == "median") x <- median(x)
-  
+
   as.POSIXlt(x)
 }
-

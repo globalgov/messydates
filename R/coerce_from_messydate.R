89f8d9e4716589cff7bd86403405488b3b8459e5
James Hollway <james.hollway@graduateinstitute.ch>
2021-07-12 16:27:17 +0200

    Merged documentation for as.Date and other methods for coercing out of messydates

--- R/coerce_from_messydate.R
+++ R/coerce_from_messydate.R
@@ -1,4 +1,4 @@
-#' Coercion from messydate to date
+#' Coercion from messy dates
 #'
 #' Coerces from messydt class to date class
 #' @param x a messydt object
@@ -6,6 +6,11 @@
 #' Before coercing dates, one should chose how to resolve ranges
 #' so that dates can be coerced to other date classes.
 #' Choices are "min","max","median", or "mean".
+#' @name from_messydate
+NULL
+#> NULL
+
+#' @rdname from_messydate
 #' @export
 as.Date.messydt <- function(x,
                             resolve = c("min","max","median","mean")){
@@ -20,14 +25,7 @@ as.Date.messydt <- function(x,
   as.Date(x)
 }
 
-#' Coercion from messydate to POSIXct
-#'
-#' Coerces from messydt class to POSIXct class
-#' @param x a messydt object
-#' @param resolve How do you want the date ranges to be resolved?
-#' Before coercing dates, one should chose how to resolve ranges
-#' so that dates can be coerced to other date classes.
-#' Choices are "min","max","median", or "mean".
+#' @rdname from_messydate
 #' @export
 as.POSIXct.messydt <- function(x,
                                resolve = c("min","max","median","mean")){
@@ -42,14 +40,7 @@ as.POSIXct.messydt <- function(x,
   as.POSIXct(x)
 }
 
-#' Coercion from messydate to POSIXlt
-#'
-#' Coerces from messydt class to POSIXlt class
-#' @param x a messydt object
-#' @param resolve How do you want the date ranges to be resolved?
-#' Before coercing dates, one should chose how to resolve ranges
-#' so that dates can be coerced to other date classes.
-#' Choices are "min","max","median", or "mean".
+#' @rdname from_messydate
 #' @export
 as.POSIXlt.messydt <- function(x,
                                resolve = c("min","max","median","mean")){

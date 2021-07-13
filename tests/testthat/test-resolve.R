350e343a368990690337ed07f83dba90064df2bb
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-07-13 12:17:16 +0200

    Fixed minor bug with resolve tests

--- tests/testthat/test-resolve.R
+++ tests/testthat/test-resolve.R
@@ -9,9 +9,9 @@ test_that("Resolve dates works properly for date ranges", {
 
 test_that("Resolve dates works properly for unspecified dates", {
   unespecified <- as_messydate("1999")
-  expect_equal(as.character(min(unspecified)), "1999-01-01")
-  expect_equal(as.character(max(unspecified)), "1999-12-31")
-  expect_equal(as.character(median(unspecified)), "1999-07-02")
-  # expect_equal(as.character(mean(unspecified)), "1999-07-02")
-  expect_equal(as.character(modal(unspecified)), "1999-01-01") # get the first when no mode?
+  expect_equal(as.character(min(unespecified)), "1999-01-01")
+  expect_equal(as.character(max(unespecified)), "1999-12-31")
+  expect_equal(as.character(median(unespecified)), "1999-07-02")
+  # expect_equal(as.character(mean(unespecified)), "1999-07-02")
+  expect_equal(as.character(modal(unespecified)), "1999-01-01") # get the first when no mode?
 })

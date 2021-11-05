b1da3c9c1cfaf1a617c82b4128072fcee1340f93
James Hollway <james.hollway@graduateinstitute.ch>
2021-11-05 09:57:18 +0100

    Added some additional precision tests

--- tests/testthat/test-extract.R
+++ tests/testthat/test-extract.R
@@ -3,3 +3,12 @@ test_that("extract functions work properly", {
   expect_equal(unclass(month(as_messydate("2012-02-03"))), 2)
   expect_equal(unclass(day(as_messydate("2012-02-03"))), 3)
 })
+
+test_that("precision function works properly", {
+  expect_equal(precision(as_messydate("2012-02-03")), 1)
+  expect_equal(precision(as_messydate("2012-02-03?")), 1)
+  expect_equal(precision(as_messydate("2012-02-03~")), 7)
+  expect_equal(precision(as_messydate("2012-02-03..2012-02-14")), 12)
+  expect_equal(precision(as_messydate("2012-02")), 29)
+  expect_equal(precision(as_messydate("2012")), 366)
+})

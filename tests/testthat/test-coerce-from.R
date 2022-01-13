ba3a8482cd0d3b96d8e4f133cb02ff31dcabb2da
Hsposito <henrique.sposito@graduateinstitute.ch>
2022-01-13 16:56:02 +0100

    Added tests for negative dates coercion

--- tests/testthat/test-coerce-from.R
+++ tests/testthat/test-coerce-from.R
@@ -5,8 +5,9 @@ test_that("Coercion from other date classes into messydt works", {
   character <- "2010-10-10"
   messy <- as_messydate("2010-10-10..2010-10-20")
   negative <- as_messydate("28 BC")
+  expect_equal(as.character(as.Date(as_messydate("1000 BC"), max)), "-1000-12-31")
   expect_equal(as.Date(messy, min), date)
   expect_equal(as.POSIXct(messy, max), POSIXct)
   expect_equal(as.POSIXlt(messy, median), POSIXlt)
-  expect_equal(as.Date(negative, min), as.Date("0028-01-01"))
+  expect_equal(as.character(as.Date(negative, min)), "-028-01-01")
 })

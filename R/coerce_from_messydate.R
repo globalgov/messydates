e1c7b0c91960eaee3689788e5e4acc052b4b061d
Hsposito <henrique.sposito@graduateinstitute.ch>
2022-01-17 14:25:08 +0100

    Added more examples for the coercion of negative dates into date format

--- R/coerce_from_messydate.R
+++ R/coerce_from_messydate.R
@@ -37,6 +37,8 @@ NULL
 #' as.Date(as_messydate("2012-01"), modal)
 #' as.Date(as_messydate("2012-01"), random)
 #' as.Date(as_messydate("1000 BC"), max)
+#' as.Date(as_messydate("1000 BC"), mean)
+#' as.Date(as_messydate("1000 BC"), median)
 #' as.Date(as_messydate(c("-1000-12-01", "2020")), min)
 #' @export
 as.Date.messydt <- function(x, ..., FUN) {

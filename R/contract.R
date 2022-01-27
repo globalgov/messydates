159ee2e8fb500653cb22839ee8dfcf28edba69dc
Hsposito <henrique.sposito@graduateinstitute.ch>
2022-01-27 14:24:39 +0100

    Updated contract() function to make it properly work with negative dates

--- R/contract.R
+++ R/contract.R
@@ -8,18 +8,29 @@
 #' @importFrom tibble tibble
 #' @importFrom lubridate NA_Date_
 #' @importFrom stringr str_replace_all str_detect
-#' @importFrom dplyr lead
+#' @importFrom dplyr lead last first
 #' @examples
 #' d <- as_messydate(c("2001-01-01", "2001-01", "2001",
-#' "2001-01-01..2001-02-02", "{2001-10-01,2001-10-04}", "{2001-01,2001-02-02}"))
+#' "2001-01-01..2001-02-02", "{2001-10-01,2001-10-04}",
+#' "{2001-01,2001-02-02}", "28 BC", "-2000-01-01"))
 #' e <- expand(d)
 #' tibble::tibble(d, contract(e))
 #' @export
 contract <- function(x) {
+  x <- compact_negative_dates(x)
   x <- compact_ranges(x)
   x <- collapse_sets(x)
   x <- collapse_ranges(x)
-  new_messydate(x)
+  as_messydate(x)
+}
+
+compact_negative_dates <- function(x) {
+  lapply(x, function(d) {
+    if (stringr::str_detect(d[1], "^-") & length(d) > 1) {
+      d <- paste0(dplyr::first(d), "..", dplyr::last(d))
+    }
+    d
+  })
 }
 
 is_sequence <- function(x) {
@@ -53,23 +64,15 @@ collapse_sets <- function(x) {
 }
 
 collapse_ranges <- function(x) {
-  x <- stringr::str_replace_all(
-    x,
-    "([:digit:]{4})-01-01\\.\\.([:digit:]{4})-12-31", "\\1")
-  x <- stringr::str_replace_all(
-    x,
-    "([:digit:]{4}-[:digit:]{2})-01\\.\\.([:digit:]{4}-[:digit:]{2})-28",
-    "\\1")
-  x <- stringr::str_replace_all(
-    x,
-    "([:digit:]{4}-[:digit:]{2})-01\\.\\.([:digit:]{4}-[:digit:]{2})-29",
-    "\\1")
-  x <- stringr::str_replace_all(
-    x,
-    "([:digit:]{4}-[:digit:]{2})-01\\.\\.([:digit:]{4}-[:digit:]{2})-30",
-    "\\1")
-  x <- stringr::str_replace_all(
-    x,
-    "([:digit:]{4}-[:digit:]{2})-01\\.\\.([:digit:]{4}-[:digit:]{2})-31",
-    "\\1")
+  x <- stringr::str_replace_all(x, "([:digit:]{4})-01-01\\.\\.([:digit:]{4})-12-31", "\\1")
+  x <- stringr::str_replace_all(x, "([:digit:]{4}-[:digit:]{2})-01\\.\\.([:digit:]{4}-[:digit:]{2})-28", "\\1")
+  x <- stringr::str_replace_all(x, "([:digit:]{4}-[:digit:]{2})-01\\.\\.([:digit:]{4}-[:digit:]{2})-29", "\\1")
+  x <- stringr::str_replace_all(x, "([:digit:]{4}-[:digit:]{2})-01\\.\\.([:digit:]{4}-[:digit:]{2})-30", "\\1")
+  x <- stringr::str_replace_all(x, "([:digit:]{4}-[:digit:]{2})-01\\.\\.([:digit:]{4}-[:digit:]{2})-31", "\\1")
+  x <- stringr::str_replace_all(x, "(-[:digit:]{4})-01-01\\.\\.(-[:digit:]{4})-12-31", "\\1")
+  x <- stringr::str_replace_all(x, "(-[:digit:]{3})-01-01\\.\\.(-[:digit:]{3})-12-31", "\\1")
+  x <- stringr::str_replace_all(x, "(-[:digit:]{4}-[:digit:]{2})-01\\.\\.(-[:digit:]{4}-[:digit:]{2})-28", "\\1")
+  x <- stringr::str_replace_all(x, "(-[:digit:]{4}-[:digit:]{2})-01\\.\\.(-[:digit:]{4}-[:digit:]{2})-29", "\\1")
+  x <- stringr::str_replace_all(x, "(-[:digit:]{4}-[:digit:]{2})-01\\.\\.(-[:digit:]{4}-[:digit:]{2})-30", "\\1")
+  x <- stringr::str_replace_all(x, "(-[:digit:]{4}-[:digit:]{2})-01\\.\\.(-[:digit:]{4}-[:digit:]{2})-31", "\\1")
 }

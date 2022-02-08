721c770e3e82ac2931e7815ddb0f73609f85ddc9
Hsposito <henrique.sposito@graduateinstitute.ch>
2022-02-08 11:59:56 +0100

    Simplified add() and subtract() functions as well as added S3 methods for using the "+" and "-" operators for messydates dates or lists

--- R/operate.R
+++ R/operate.R
@@ -4,286 +4,83 @@
 #' Messydate objects include incomplete or uncertain dates,
 #' ranges of dates, negative dates, and date sets.
 #' @param x A date or list of dates
-#' @param digit How many date units?
-#' @param component What date component?
-#' By default year.
-#' Other options are month and day.
+#' @param n What do you want to be added?
+#' Please specify in numeric format (i.e. one day is 1, one year is 365).
 #' @importFrom stringr str_detect str_replace str_split
 #' @importFrom lubridate as_date
 #' @return A messydates vector
+#' @details `add()` adds date units to dates, negative dates,
+#' ranges of dates, and sets of dates.
+#' For ranges of dates, it increases the range by the unit on each side.
+#' `subtract()` subtracts date units from dates, negative dates,
+#' ranges of dates, and sets of dates.
+#' For ranges of dates, it reduces the range by the unit on each side.
 #' @examples
 #' d <- as_messydate(c("2008-03-25", "-2012-02-27", "2001-01?", "~2001",
 #' "2001-01-01..2001-02-02", "{2001-01-01,2001-02-02}",
 #' "2008-XX-31", "..2002-02-03", "2001-01-03..", "28 BC"))
-#' tibble::tibble(date = d, day = add(d, 1, "day"), month = add(d, 1, "month"),
-#' year = add(d, 1))
-#' tibble::tibble(date = d, day = subtract(d, 1, "day"), month = subtract(d, 1, "month"),
-#' year = subtract(d, 1))
-#' @name operate
-
-#' @rdname operate
-#' @details `add()` adds date units to dates, negative dates,
-#' ranges of dates, and sets of dates.
-#' For ranges of dates, it increases the range by the unit on each side.
-#' @export
-add <- function(x, digit, component = "year") {
+#' tibble::tibble(date = d, add = d + 1, subtract = d - 1)
+add <- function(x, n) {
   # Step one: get only first and last components for ranges
   # But keep approximation for before or after date
-  x <- ifelse(stringr::str_detect(x, "^\\.\\.|\\.\\.$"), x,
-              contract(expand(x), collapse = FALSE))
-  x <- stringr::str_remove_all(x, "\\{|\\}")
+  x <- ifelse(stringr::str_detect(x, "^\\.\\.|\\.\\.$"), x, expand(x))
   # Step two, add by component
-  if (component == "year") {
-    ay <- as.numeric(strsplit(as.character(digit/4), "\\.")[[1]][1]) + (365*digit)
-    x <- sapply(x, function(y) {
-      a <- stringr::str_split(y, "\\.\\.")[[1]][1]
-      b <- stringr::str_split(y, "\\.\\.")[[1]][2]
-      if (stringr::str_detect(y[1], "[:digit:]{2}\\.\\.\\-[:digit:]{4}")) {
-          y <- paste0("-", lubridate::as_date(a) + ay, "..", "-", lubridate::as_date(b) - ay)
-      }
-      if (stringr::str_detect(y[1], "^-") &
-          stringr::str_detect(y[1], "\\.\\.", negate = TRUE)) {
-        y <- paste0("-", lubridate::as_date(y) + ay)
-      }
-      if (stringr::str_detect(y[1], "[:digit:]{2}\\.\\.[:digit:]{4}")) {
-        y <- paste0(lubridate::as_date(a) - ay, "..", lubridate::as_date(b) + ay)
-      }
-      if (stringr::str_detect(y[1], "^-", negate = TRUE) &
-          stringr::str_detect(y[1], "\\.\\.", negate = TRUE) &
-          stringr::str_detect(y[1], "\\,", negate = TRUE)) {
-        y <- paste0(lubridate::as_date(y) + ay)
-      }
-      if (stringr::str_detect(y[1], "\\,")) {
-        y <- stringr::str_split(y, "\\,")
-        y <- lapply(y, function(s) {
-          as.character(lubridate::as_date(s) + ay)
-        })
-        y <- paste(y, sep = ",")
-      }
-      if (stringr::str_detect(y[1], "^\\.\\.")) {
-        y <- stringr::str_remove(y, "\\.\\.")
-        y <- paste0("..", lubridate::as_date(y) + ay)
-      }
-      if (stringr::str_detect(y[1], "\\.\\.$")) {
-        y <- stringr::str_remove(y, "\\.\\.")
-        y <- paste0(lubridate::as_date(y) + ay, "..")
-      }
-      y
+  x <- sapply(x, function(y) {
+    if (stringr::str_detect(y[1], "^-")) {
+      y <- paste0("-", lubridate::as_date(y) - n)
+    } else if (stringr::str_detect(y[1], "^\\.\\.")) {
+      y <- stringr::str_remove(y, "\\.\\.")
+      y <- paste0("..", lubridate::as_date(y) + n)
+    } else if (stringr::str_detect(y[1], "\\.\\.$")) {
+      y <- stringr::str_remove(y, "\\.\\.")
+      y <- paste0(lubridate::as_date(y) + n, "..")
+    } else {
+      y <- lubridate::as_date(y) + n
+    }
+    y
     })
-  }
-  if (component == "month") {
-    m <- 30.42 * digit
-    x <- sapply(x, function(y) {
-      a <- stringr::str_split(y, "\\.\\.|\\,")[[1]][1]
-      b <- stringr::str_split(y, "\\.\\.|\\,")[[1]][2]
-      if (stringr::str_detect(y[1], "^-") & stringr::str_detect(y[1], "\\.\\.")) {
-        y <- paste0("-", lubridate::as_date(a) + m, "..", "-", lubridate::as_date(b) - m)
-      }
-      if (stringr::str_detect(y[1], "^-") &
-          stringr::str_detect(y[1], "\\.\\.", negate = TRUE)) {
-        y <- paste0("-", lubridate::as_date(y) + m)
-      }
-      if (stringr::str_detect(y[1], "^-", negate = TRUE) &
-          stringr::str_detect(y[1], "\\.\\.")) {
-        y <- paste0(lubridate::as_date(a) - m, "..", lubridate::as_date(b) + m)
-      }
-      if (stringr::str_detect(y[1], "^-", negate = TRUE) &
-          stringr::str_detect(y[1], "\\.\\.", negate = TRUE) &
-          stringr::str_detect(y[1], "\\,", negate = TRUE)) {
-        y <- paste0(as.Date(y) + m)
-      }
-      if (stringr::str_detect(y[1], "\\,")) {
-        y <- stringr::str_split(y, "\\,")
-        y <- lapply(y, function(s) {
-          as.character(lubridate::as_date(s) + m)
-        })
-        y <- paste(y, sep = ",")
-      }
-      if (stringr::str_detect(y[1], "^\\.\\.")) {
-        y <- stringr::str_remove(y, "\\.\\.")
-        y <- paste0("..", lubridate::as_date(y) + m)
-      }
-      if (stringr::str_detect(y[1], "\\.\\.$")) {
-        y <- stringr::str_remove(y, "\\.\\.")
-        y <- paste0(lubridate::as_date(y) + m, "..")
-      }
-      y
-    })
-  }
-  if (component == "day") {
-    d <- digit
-    x <- sapply(x, function(y) {
-      a <- stringr::str_split(y, "\\.\\.|\\,")[[1]][1]
-      b <- stringr::str_split(y, "\\.\\.|\\,")[[1]][2]
-      if (stringr::str_detect(y[1], "^-") & stringr::str_detect(y[1], "\\.\\.")) {
-        y <- paste0("-", lubridate::as_date(a) + d, "..", "-", lubridate::as_date(b) - d)
-      }
-      if (stringr::str_detect(y[1], "^-") &
-          stringr::str_detect(y[1], "\\.\\.", negate = TRUE)) {
-        y <- paste0("-", lubridate::as_date(y) + d)
-      }
-      if (stringr::str_detect(y[1], "^-", negate = TRUE) &
-          stringr::str_detect(y[1], "\\.\\.")) {
-        y <- paste0(lubridate::as_date(a) - d, "..", lubridate::as_date(b) + d)
-      }
-      if (stringr::str_detect(y[1], "^-", negate = TRUE) &
-          stringr::str_detect(y[1], "\\.\\.", negate = TRUE) &
-          stringr::str_detect(y[1], "\\,", negate = TRUE)) {
-        y <- paste0(as.Date(y) + d)
-      }
-      if (stringr::str_detect(y[1], "\\,")) {
-        y <- stringr::str_split(y, "\\,")
-        y <- lapply(y, function(s) {
-          as.character(lubridate::as_date(s) + d)
-        })
-        y <- paste(y, sep = ",")
-      }
-      if (stringr::str_detect(y[1], "^\\.\\.")) {
-        y <- stringr::str_remove(y, "\\.\\.")
-        y <- paste0("..", lubridate::as_date(y) + d)
-      }
-      if (stringr::str_detect(y[1], "\\.\\.$")) {
-        y <- stringr::str_remove(y, "\\.\\.")
-        y <- paste0(lubridate::as_date(y) + d, "..")
-      }
-      y
-    })
-  }
-  x <- ifelse(stringr::str_detect(x, ","), paste0("{", x, "}"), x)
-  x <- stringr::str_remove_all(x, "c\\(|\\)")
-  x <- stringr::str_remove_all(x, '"')
-  as_messydate(x)
+  contract(x)
 }
 
-#' @rdname operate
-#' @details `subtract()` subtracts date units from dates, negative dates,
-#' ranges of dates, and sets of dates.
-#' For ranges of dates, it reduces the range by the unit on each side.
-#' @export
-subtract <- function(x, digit, component = "year") {
+subtract <- function(x, n) {
   # Step one: get only first and last components for ranges
-  x <- ifelse(stringr::str_detect(x, "^\\.\\.|\\.\\.$"), x,
-              contract(expand(x), collapse = FALSE))
-  x <- stringr::str_remove_all(x, "\\{|\\}")
-  # Step two, subtract by component
-  if (component == "year") {
-    ay <- as.numeric(strsplit(as.character(digit/4), "\\.")[[1]][1]) + (365*digit)
-    x <- sapply(x, function(y) {
-      a <- stringr::str_split(y, "\\.\\.")[[1]][1]
-      b <- stringr::str_split(y, "\\.\\.")[[1]][2]
-      if (stringr::str_detect(y[1], "[:digit:]{2}\\.\\.\\-[:digit:]{4}")) {
-        y <- paste0("-", lubridate::as_date(a) - ay, "..", "-", lubridate::as_date(b) + ay)
-      }
-      if (stringr::str_detect(y[1], "^-") &
-          stringr::str_detect(y[1], "\\.\\.", negate = TRUE)) {
-        y <- paste0("-", lubridate::as_date(y) - ay)
-      }
-      if (stringr::str_detect(y[1], "[:digit:]{2}\\.\\.[:digit:]{4}")) {
-        y <- paste0(lubridate::as_date(a) + ay, "..", lubridate::as_date(b) - ay)
-      }
-      if (stringr::str_detect(y[1], "^-", negate = TRUE) &
-          stringr::str_detect(y[1], "\\.\\.", negate = TRUE) &
-          stringr::str_detect(y[1], "\\,", negate = TRUE)) {
-        y <- paste0(lubridate::as_date(y) - ay)
-      }
-      if (stringr::str_detect(y[1], "\\,")) {
-        y <- stringr::str_split(y, "\\,")
-        y <- lapply(y, function(s) {
-          as.character(lubridate::as_date(s) - ay)
-        })
-        y <- paste(y, sep = ",")
-      }
-      if (stringr::str_detect(y[1], "^\\.\\.")) {
-        y <- stringr::str_remove(y, "\\.\\.")
-        y <- paste0("..", lubridate::as_date(y) - ay)
-      }
-      if (stringr::str_detect(y[1], "\\.\\.$")) {
-        y <- stringr::str_remove(y, "\\.\\.")
-        y <- paste0(lubridate::as_date(y) - ay, "..")
-      }
-      y
-    })
-  }
-  if (component == "month") {
-    m <- 30.42 * digit
-    x <- sapply(x, function(y) {
-      a <- stringr::str_split(y, "\\.\\.|\\,")[[1]][1]
-      b <- stringr::str_split(y, "\\.\\.|\\,")[[1]][2]
-      if (stringr::str_detect(y[1], "^-") & stringr::str_detect(y[1], "\\.\\.")) {
-        y <- paste0("-", lubridate::as_date(a) - m, "..", "-", lubridate::as_date(b) + m)
-      }
-      if (stringr::str_detect(y[1], "^-") &
-          stringr::str_detect(y[1], "\\.\\.", negate = TRUE)) {
-        y <- paste0("-", lubridate::as_date(y) - m)
-      }
-      if (stringr::str_detect(y[1], "^-", negate = TRUE) &
-          stringr::str_detect(y[1], "\\.\\.")) {
-        y <- paste0(lubridate::as_date(a) + m, "..", lubridate::as_date(b) - m)
-      }
-      if (stringr::str_detect(y[1], "^-", negate = TRUE) &
-          stringr::str_detect(y[1], "\\.\\.", negate = TRUE) &
-          stringr::str_detect(y[1], "\\,", negate = TRUE)) {
-        y <- paste0(lubridate::as_date(y) - m)
-      }
-      if (stringr::str_detect(y[1], "\\,")) {
-        y <- stringr::str_split(y, "\\,")
-        y <- lapply(y, function(s) {
-          as.character(lubridate::as_date(s) - m)
-        })
-        y <- paste(y, sep = ",")
-      }
-      if (stringr::str_detect(y[1], "^\\.\\.")) {
-        y <- stringr::str_remove(y, "\\.\\.")
-        y <- paste0("..", lubridate::as_date(y) - m)
-      }
-      if (stringr::str_detect(y[1], "\\.\\.$")) {
-        y <- stringr::str_remove(y, "\\.\\.")
-        y <- paste0(lubridate::as_date(y) - m, "..")
-      }
-      y
-    })
-  }
-  if (component == "day") {
-    d <- digit
-    x <- sapply(x, function(y) {
-      a <- stringr::str_split(y, "\\.\\.|\\,")[[1]][1]
-      b <- stringr::str_split(y, "\\.\\.|\\,")[[1]][2]
-      if (stringr::str_detect(y[1], "^-") & stringr::str_detect(y[1], "\\.\\.")) {
-        y <- paste0("-", lubridate::as_date(a) - d, "..", "-", lubridate::as_date(b) + d)
-      }
-      if (stringr::str_detect(y[1], "^-") &
-          stringr::str_detect(y[1], "\\.\\.", negate = TRUE)) {
-        y <- paste0("-", lubridate::as_date(y) - d)
-      }
-      if (stringr::str_detect(y[1], "^-", negate = TRUE) &
-          stringr::str_detect(y[1], "\\.\\.")) {
-        y <- paste0(lubridate::as_date(a) + d, "..", lubridate::as_date(b) - d)
-      }
-      if (stringr::str_detect(y[1], "^-", negate = TRUE) &
-          stringr::str_detect(y[1], "\\.\\.", negate = TRUE) &
-          stringr::str_detect(y[1], "\\,", negate = TRUE)) {
-        y <- paste0(lubridate::as_date(y) - d)
-      }
-      if (stringr::str_detect(y[1], "\\,")) {
-        y <- stringr::str_split(y, "\\,")
-        y <- lapply(y, function(s) {
-          as.character(lubridate::as_date(s) - d)
-        })
-        y <- paste(y, sep = ",")
-      }
-      if (stringr::str_detect(y[1], "^\\.\\.")) {
-        y <- stringr::str_remove(y, "\\.\\.")
-        y <- paste0("..", lubridate::as_date(y) - d)
-      }
-      if (stringr::str_detect(y[1], "\\.\\.$")) {
-        y <- stringr::str_remove(y, "\\.\\.")
-        y <- paste0(lubridate::as_date(y) - d, "..")
-      }
-      y
-    })
-  }
-  x <- ifelse(stringr::str_detect(x, ","), paste0("{", x, "}"), x)
-  x <- stringr::str_remove_all(x, "c\\(|\\)")
-  x <- stringr::str_remove_all(x, '"')
-  as_messydate(x)
+  # But keep approximation for before or after date
+  x <- ifelse(stringr::str_detect(x, "^\\.\\.|\\.\\.$"), x, expand(x))
+  # Step two, add by component
+  x <- sapply(x, function(y) {
+    if (stringr::str_detect(y[1], "^-")) {
+      y <- paste0("-", lubridate::as_date(y) + n)
+    } else if (stringr::str_detect(y[1], "^\\.\\.")) {
+      y <- stringr::str_remove(y, "\\.\\.")
+      y <- paste0("..", lubridate::as_date(y) - n)
+    } else if (stringr::str_detect(y[1], "\\.\\.$")) {
+      y <- stringr::str_remove(y, "\\.\\.")
+      y <- paste0(lubridate::as_date(y) - n, "..")
+    } else {
+      y <- lubridate::as_date(y) - n
+    }
+    y
+  })
+  contract(x)
+}
+
+#' #' @include class.R
+#' #' @export
+#' setMethod("+", signature(e1 = "messydt", e2 = "numeric"),
+#'           function(e1, e2) add(e1, e2))
+#'
+#' #' @include class.R
+#' #' @export
+#' setMethod("-", signature(e1 = "messydt", e2 = "numeric"),
+#'           function(e1, e2) subtract(e1, e2))
+
+#' @param e1 element one
+#' @param e2 element two
+#' @export
+`+.messydt` <- function(e1, e2){
+  add(e1, e2)
+}
+#' @export
+`-.messydt` <- function(e1, e2){
+  subtract(e1, e2)
 }

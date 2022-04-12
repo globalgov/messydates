b6fce03f3ebe6a11be886eda2fd51e04d966edcd
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2022-04-12 16:28:04 +0200

    Renamed some test files for consistency

--- tests/testthat/test-skimr.R
+++ tests/testthat/test-skimr.R
@@ -3,10 +3,11 @@ d <- tibble::tibble(event = c("Event1", "Event2", "Event3", "Event 4"),
                                                  "2001-01-01..2003-12-30",
                                                  "{2001, 2002, 2003}",
                                                  "33 BC")))
+
 test_that("skimr report works", {
   a <- skimr::skim(d)
   expect_equal(a$skim_type, c("character", "messydt"))
   expect_equal(a$skim_variable, c("event", "messydates"))
   expect_equal(a$n_missing, c(0, 0))
-  expect_length(a, 11)
+  expect_length(a, 14)
 })

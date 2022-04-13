f8b03cc4d9a2b752580e57951df44fd5ab450b49
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2022-04-13 14:07:26 +0200

    Fixed a few code factor issues

--- tests/testthat/test-coerce-to.R
+++ tests/testthat/test-coerce-to.R
@@ -4,9 +4,9 @@ test_that("Coercion from other date classes into messydt works", {
   POSIXlt <- as.POSIXlt("2010-10-10")
   character <- "2010-10-10"
   character2 <- "AD2010-10-10"
-  character3 <- c("{BC2010-10-10,BC2010-10-11,BC2010-10-12}")
+  character3 <- "{BC2010-10-10,BC2010-10-11,BC2010-10-12}"
   messy <- as_messydate("2010-10-10")
-  messyneg <- as_messydate(c("{-2010-10-10,-2010-10-11,-2010-10-12}"))
+  messyneg <- as_messydate("{-2010-10-10,-2010-10-11,-2010-10-12}")
   expect_equal(as_messydate(date), messy)
   expect_equal(as_messydate(POSIXct), messy)
   expect_equal(as_messydate(POSIXlt), messy)
--- tests/testthat/test-messydate-make.R
+++ tests/testthat/test-messydate-make.R
@@ -4,5 +4,4 @@ test_that("mutiple variables are properly bind", {
   expect_equal(make_messydate("2010-01-01"), as_messydate("2010-1-1"))
   expect_equal(make_messydate("2012?", "01", "01"), as_messydate("2012?-01-01"))
   expect_equal(make_messydate("2012", "01", "01~"), as_messydate("2012-01-01~"))
-  expect_error(make_messydate(c("2010"), c("10")))
 })

0c260315580beafc6671ce7616716f8c3a08db3c
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2022-05-02 12:54:32 +0200

    Deleted text_to_date() function. This functionality has been incorporated into as_messydate() function.

--- tests/testthat/test-text-to-date.R
+++
@@ -1,5 +0,0 @@
-test_that("dates are properly extracted from text", {
-  expect_equal(text_to_date(c("This function was created on the 29 September 2021",
-                               "Today is October 12, 2021", "Tomorrow is 13-10-2021")),
-               as_messydate(c("29-9-2021", "12-10-2021", "13-10-2021")))
-})

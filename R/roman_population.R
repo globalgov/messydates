7d2f818201a6abcce1ddc678ccf52809506cd20f
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2022-05-24 21:39:05 +0200

    Added roman_population dataset

---
+++ R/roman_population.R
@@ -0,0 +1,16 @@
+#' Roman Population Sizes dataset
+#'
+#' The population sizes in the Roman Empire has been a topic of heated debates among some academics.
+#' Various accounts diverge in how they estimate population size and, therefore,
+#' the conclusions about the Roman population at different points in time (see Scheidel2008).
+#' This dataset contains the high and low estimations for the population of mainland
+#' Italy during the Roman Empire (scheidel2008, p. 31).
+#' @source Scheidel, W. (2008). Roman population size: the logic of the debate.
+#' People, land, and politics: demographic developments and the transformation of Roman Italy, 300, 17-70.
+#' @format A data frame with 4 rows and 3 variables:
+#' \describe{
+#'   \item{period}{date range of the Roman Empire}
+#'   \item{population_min}{minimum population estimate}
+#'   \item{population_max}{maximum population estimate}
+#' }
+"roman_population"

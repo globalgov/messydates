3015b84756814f323036eff1d2d7117276dc86d1
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2022-05-25 15:12:20 +0200

    Deleted roman_population data

--- data-raw/roman_population.R
+++
@@ -1,5 +0,0 @@
-# Roman population size estimates
-roman_population <- tibble::tibble(period = c("200 BC:100 BC", "100 BC:1 AD", "1 AD:150 AD", "150 AD:200 AD"),
-                                   population_min = c(3900000, 4400000, 5500000, 6000000),
-                                   population_max = c(8000000, 10000000, 16000000, 2000000))
-usethis::use_data(roman_population, overwrite = TRUE)

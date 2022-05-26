37d1d9441ad6001b0110e094adfde76495b5c55d
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2022-05-26 10:33:57 +0200

    Updated logical functions documentation so that functions are properly displayed in file

--- R/logical.R
+++ R/logical.R
@@ -6,6 +6,11 @@
 #' @return A logical vector the same length as the `mdate` passed.
 NULL
 
+#' Logical tests on messy dates
+#'
+#' These functions provide various logical tests for messy date objects.
+#' @param x,y `mdate` or other class objects
+#' @return A logical vector the same length as the `mdate` passed
 #' @describeIn logical tests whether the object inherits the `mdate` class.
 #'   If more rigorous validation is required, see `validate_messydate()`.
 #' @examples

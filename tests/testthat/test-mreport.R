c5cc9b4266e4ab76f989a964c100c3eaa678818e
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2022-07-05 13:14:44 +0200

    Added more tests for mreport() function

--- tests/testthat/test-mreport.R
+++
@@ -1,8 +0,0 @@
-test_that("report function work properly", {
-  report <- mreport(battles)
-  type <- c("character", "mdate")
-  names(type) <- c("Battle", "Date")
-  expect_s3_class(report, "mreport")
-  expect_equal(report$Types, type)
-  expect_length(report, 9)
-})

7c3d2ecfa05bcb64be8a9cfcba394eb19ae85f2d
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2023-08-22 12:16:41 +0200

    Added more tests for proportions

--- tests/testthat/test-proportional.R
+++ tests/testthat/test-proportional.R
@@ -9,8 +9,8 @@ test_that("proportional methods work", {
   expect_equal(round(as_messydate("2012-06") %leqq%
                        as_messydate("2012-06-10"), 2), 0.33)
   expect_equal(as_messydate("2012-06") %geqq% as_messydate("2012-06-10"), 0.70)
-  # expect_equal(round(as_messydate("2012-06") %><%
-  #                      as_messydate("2012-06-15..2012-07-15"), 2), 0.50)
-  # expect_equal(round(as_messydate("2012-06") %>=<%
-  #                      as_messydate("2012-06-15..2012-07-15"), 2), 0.50)
+  expect_equal(round(as_messydate("2012-06") %><%
+                       as_messydate("2012-06-15..2012-07-15"), 2), 0.52)
+  expect_equal(round(as_messydate("2012-06") %>=<%
+                       as_messydate("2012-06-15..2012-07-15"), 2), 0.53)
 })

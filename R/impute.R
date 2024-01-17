593ef4101c330381079e6ed65cda44416adfb549
James Hollway <james.hollway@graduateinstitute.ch>
2024-01-17 15:53:25 +0100

    Added to_pool() for creating datasets pooling imputations in preparation for Cox models

--- R/impute.R
+++ R/impute.R
@@ -7,3 +7,16 @@ impute <- function(.data, times = 1000) {
   })
 }
 
+
+#' @examples
+#' library(psfmi)
+#' res <- to_pool(impute(battles, 10)) |>
+#'   mutate(Date = as.numeric(Date)) |>
+#'   psfmi_coxr(Surv(Date, status) ~ US_party + N_actors, impvar = "impute_num")
+#' res$RR_model_final
+#' @export
+to_pool <- function(imputes){
+  dplyr::bind_rows(imputes, .id = "impute_num") |>
+    dplyr::mutate(impute_num = as.numeric(impute_num),
+                  status = 1)
+}

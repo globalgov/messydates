85477fcec17ca0dfc3e0633843af4c9f93f0d7bb
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2024-01-18 11:28:07 -0300

    Reverte changes with resolve functions to avoid test failures

--- R/impute.R
+++ R/impute.R
@@ -7,15 +7,15 @@ impute <- function(.data, times = 1000) {
   })
 }
 
-
 #' @examples
 #' library(psfmi)
 #' res <- to_pool(impute(battles, 10)) |>
-#'   mutate(Date = as.numeric(Date)) |>
+#'   dplyr::mutate(Date = as.numeric(Date)) |>
 #'   psfmi_coxr(Surv(Date, status) ~ US_party + N_actors, impvar = "impute_num")
 #' res$RR_model_final
 #' @export
-to_pool <- function(imputes){
+to_pool <- function(imputes) {
+  impute_num <- NULL
   dplyr::bind_rows(imputes, .id = "impute_num") |>
     dplyr::mutate(impute_num = as.numeric(impute_num),
                   status = 1)

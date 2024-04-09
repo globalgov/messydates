2a9a2ccca3d21a79d42dfd797cc15e598b01a150
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2024-04-09 19:09:35 +0200

    Added additional tests for logical comparisons

--- tests/testthat/test-logical.R
+++ tests/testthat/test-logical.R
@@ -68,6 +68,7 @@ test_that("Logical comparisons work", {
   expect_true(as_messydate("-0001-12-31") <= as_messydate("0001-01-01"))
   expect_false(as_messydate("1 BC") > as_messydate("1 AC"))
   expect_true(as_messydate("79") < as_messydate("0193-03"))
+  expect_true(as_messydate("39-01-01") < as_messydate("100-12-01"))
 })
 
 test_that("Logical comparisons don't mess up comparisons between non-messy times", {

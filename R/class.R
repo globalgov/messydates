03045219e05030477fa444caa677da3ff865309c
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2024-04-11 11:10:15 +0200

    Reverted changes to data frame subsetting for 'mdate' objects

--- R/class.R
+++ R/class.R
@@ -126,7 +126,7 @@ c.mdate <- function(...) {
 
 #' @export
 as.data.frame.mdate <- function(x, ...) {
-  nm <- deparse(substitute(x))
+  nm <- deparse1(substitute(x))
   if (!"nm" %in% ...names())
     as.data.frame.vector(x, ..., nm = nm)
   else as.data.frame.vector(x, ...)

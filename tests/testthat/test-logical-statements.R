d8da13f70ef3ce1a146290ebd7a8f0ea99173431
James Hollway <james.hollway@graduateinstitute.ch>
2025-02-11 22:26:17 +0100

    Cut many tests to accelerate testing procedure

---
+++ tests/testthat/test-logical-statements.R
@@ -0,0 +1,38 @@
+# Testing logical methods of messydates
+test_that("is_messydate works", {
+  expect_true(is_messydate(as_messydate("2012-01-01")))
+  expect_false(is_messydate("2012-01-01"))
+})
+
+test_that("is_intersect works", {
+  expect_true(is_intersecting(as_messydate("2012-01"),  as_messydate("2012-01-01..2012-02-22")))
+  # expect_false(is_intersecting(as_messydate("2012-01"),
+  #                              as_messydate("2012-02-01..2012-02-22")))
+})
+
+test_that("is_subset works", {
+  expect_true(is_subset(as_messydate("2012-01-01"), as_messydate("2012-01")))
+  expect_true(is_subset(as_messydate("2012-01-01..2012-01-03"), as_messydate("2012-01")))
+  # expect_false(is_subset(as_messydate("2012-01-01"), as_messydate("2012-02")))
+})
+
+test_that("is_similar works", {
+  expect_true(is_similar(as_messydate("2012-06-02"), as_messydate("2012-02-06")))
+  # expect_false(is_similar(as_messydate("2012-06-22"), as_messydate("2012-02-06")))
+})
+
+test_that("is_precise works", {
+  expect_true(is_precise(as_messydate("2012-06-02")))
+  # expect_false(is_precise(as_messydate("2012?-06-22")))
+})
+
+test_that("is_uncertain works", {
+  expect_true(is_uncertain(as_messydate("2012-06-02?")))
+  # expect_false(is_uncertain(as_messydate("2012-06-22")))
+})
+
+test_that("is_approximate works", {
+  expect_true(is_approximate(as_messydate("2012-06-02~")))
+  # expect_false(is_approximate(as_messydate("2012-06-22")))
+})
+
